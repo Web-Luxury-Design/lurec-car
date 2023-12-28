981b60bf07bc5d757ea9965acf74c8bb3e334f48
Test <adrienlegeleu@gmail.com>
2023-12-28 17:02:19 +0100

    lurex

--- lurex/src/components/Header.js
+++ lurex/src/components/Header.js
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Header = () => {
+  const [isTransformed, setIsTransformed] = useState(false);
+
+  useEffect(() => {
+    const home = document.querySelector(".home img");
+    const nav = document.querySelector(".nav");
+
+    const homeShow = () => {
+      if (isTransformed) {
+        home.style.transform = "translateX(-200px)";
+        home.style.transition = "0.8s ease-in-out";
+        nav.style.transform = "translateX(0)";
+        setIsTransformed(false);
+      } else {
+        home.style.transform = "translateX(0)";
+        nav.style.transform = "translateX(120%)";
+        setIsTransformed(true);
+      }
+    };
+
+    if (home) {
+      home.addEventListener("click", homeShow);
+
+      return () => {
+        home.removeEventListener("click", homeShow);
+      };
+    }
+  }, [isTransformed]);
+
   return (
     <div className="header">
       <div className="logo">
@@ -8,11 +36,14 @@ const Header = () => {
       </div>
       <ul className="nav">
         <li>Home</li>
-        <li>cars</li>
+        <li>Cars</li>
         <li>About us</li>
         <li>Booking</li>
         <button>Contact</button>
       </ul>
+      <div className="home">
+        <img src="./img/sport-car_834304.png" alt="" />
+      </div>
     </div>
   );
 };
